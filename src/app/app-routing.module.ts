122893f22c1738fa379a91cde34e64a5583a24a0
ivanretamozo <retamozoi445@gmail.com>
2022-07-11 16:39:22 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
+import { AuthPageComponent } from './modules/auth/pages/auth-page/auth-page.component';
 
 const routes: Routes = [
   {
+    path: "auth",
+    loadChildren: ()=> import("@modules/auth/auth.module").then(m => m.AuthModule)
+  },
+  {
     path: "",
-    loadChildren: ()=> import("./modules/home/home.module").then(m => m.HomeModule)
+    component: HomePageComponent,
+    loadChildren: ()=> import("@modules/home/home.module").then(m => m.HomeModule)
   }
 ];
 
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { TracksModule } from '../tracks/tracks.module';
+
 
 const routes: Routes = [
   {
-    path: "",
-    component: HomePageComponent
+    path: "tracks",
+    loadChildren: ()=> import("@modules/tracks/tracks.module").then(m=> TracksModule)
+  },
+  {
+    path: "history",
+    loadChildren: ()=> import("@modules/history/history.module").then(m=> m.HistoryModule)
+  },
+  {
+    path: "favorites",
+    loadChildren: ()=> import("@modules/favorites/favorites.module").then(m=> m.FavoritesModule)
   }
 ];
 
