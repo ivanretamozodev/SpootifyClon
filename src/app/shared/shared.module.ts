91925f21f23e042279b24ddde05ddda9c5a9e433
ivanretamozo <retamozoi445@gmail.com>
2022-07-12 22:14:11 -0300

    working in generic section of tracks

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { UserHeaderComponent } from './components/user-header/user-header.component';
+
+import { CardPlayerComponent } from './components/card-player/card-player.component';
 import { MediaPlayerComponent } from './components/media-player/media-player.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { GenericSectionComponent } from './components/generic-section/generic-section.component';
+import { UserHeaderComponent } from './components/user-header/user-header.component';
+
 
 
 
 @NgModule({
   declarations: [
+    CardPlayerComponent,
+    GenericSectionComponent,
+    MediaPlayerComponent,
     SideBarComponent,
     UserHeaderComponent,
-    MediaPlayerComponent
+  
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    CardPlayerComponent,
+    GenericSectionComponent,
+    MediaPlayerComponent,
     SideBarComponent,
-    MediaPlayerComponent
+    UserHeaderComponent,
   ]
 })
 export class SharedModule { }
